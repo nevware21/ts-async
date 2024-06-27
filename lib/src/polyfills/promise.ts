feb198c231a7353002b1cf0fc2cd58da1552d5a6
Nev <nev@nevware21.com>
2024-06-27 00:25:16 -0700

    Bump typedoc from 0.25.13 to 0.26.2

--- lib/src/polyfills/promise.ts
+++ lib/src/polyfills/promise.ts
@@ -23,7 +23,7 @@ export interface PolyPromiseConstructor {
     /**
      * Creates a new Promise.
      * @constructor
-     * @param executor A callback used to initialize the promise. This callback is passed two arguments:
+     * @param executor - A callback used to initialize the promise. This callback is passed two arguments:
      * a resolve callback used to resolve the promise with a value or the result of another promise,
      * and a reject callback used to reject the promise with a provided reason or error.
      */
